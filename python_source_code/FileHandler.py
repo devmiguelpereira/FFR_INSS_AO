805f78cb6dcf8ea8776435b19130aa2bead00d1d
EIS-MPEREIRA <101540483+EIS-MPEREIRA@users.noreply.github.com>
2023-02-18 16:00:14 +0100

    perf(import_from_file): handles additional exception types
    Here are a few possible improvements to the code:  Using a dictionary to

--- python_source_code/FileHandler.py
+++ python_source_code/FileHandler.py
@@ -10,31 +10,47 @@ file_path = 'november_inss_2022.csv'
 
 
 def import_from_file(file_path):
-    
+    error_messages = {
+        FileNotFoundError: f"Error: file {file_path} not found.",
+        csv.Error: f"Error reading CSV file {file_path}: %s",
+        pd.errors.ParserError: f"Error reading Excel file {file_path}: %s",
+        Exception: f"Error reading file {file_path}: %s",
+        KeyError: f"Error: header row not found in {file_path}."
+    }
+
     if not os.path.isfile(file_path):
-        print(f"Error: file {file_path} not found.")
+        print(error_messages[FileNotFoundError])
         return None
 
     _, file_extension = os.path.splitext(file_path)
     if file_extension == '.csv':
-        with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            headers = next(reader)  # skip the header row
-            employee_data_list = [row for row in reader]
-        return employee_data_list
-
-    elif file_extension == '.xlsx' or file_extension == '.xls':
         try:
-            employee_data = pd.read_excel(file_path, engine='openpyxl')
-            employee_data_list = employee_data.values.tolist()
+            with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                headers = next(reader)  # skip the header row
+                employee_data_list = [row for row in reader]
             return employee_data_list
+        except tuple(error_messages.keys()) as e:
+            print(error_messages[type(e)] % str(e))
         except Exception as e:
-            print(f"Error reading Excel file {file_path}: {e}")
-            return None
+            print(error_messages[Exception] % str(e))
+
+    elif file_extension in ('.xlsx', '.xls'):
+        try:
+            with pd.ExcelFile(file_path, engine='openpyxl') as xls:
+                sheet_name = xls.sheet_names[0]
+                employee_data = pd.read_excel(xls, sheet_name)
+                employee_data_list = employee_data.values.tolist()
+            return employee_data_list
+        except tuple(error_messages.keys()) as e:
+            print(error_messages[type(e)] % str(e))
+        except Exception as e:
+            print(error_messages[Exception] % str(e))
 
     else:
         print(f"Error: file {file_path} is not a CSV or Excel file.")
         return None
+
 
 
 def create_file():
