7d16833ce57f1974fe2c8e2231b49c6e4328268d
EIS-MPEREIRA <101540483+EIS-MPEREIRA@users.noreply.github.com>
2023-02-18 16:17:49 +0100

    feat(select_file): Select_file, create_file, import_from_file
    When this function is called, it will open a file dialog window that

--- python_source_code/FileHandler.py
+++ python_source_code/FileHandler.py
@@ -4,22 +4,23 @@ import os
 import FileLayout as fl
 import pandas as pd
 
-file_path = 'november_inss_2022.csv'
+import tkinter as tk
+from tkinter import filedialog
 
-# imports data from a csv file
+
+# Selects a file on the directory
+def select_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    return file_path
 
 
+# imports data from a csv or excel file
 def import_from_file(file_path):
-    error_messages = {
-        FileNotFoundError: f"Error: file {file_path} not found.",
-        csv.Error: f"Error reading CSV file {file_path}: %s",
-        pd.errors.ParserError: f"Error reading Excel file {file_path}: %s",
-        Exception: f"Error reading file {file_path}: %s",
-        KeyError: f"Error: header row not found in {file_path}."
-    }
 
     if not os.path.isfile(file_path):
-        print(error_messages[FileNotFoundError])
+        print(f"Error: file {file_path} not found.")
         return None
 
     _, file_extension = os.path.splitext(file_path)
@@ -30,44 +31,53 @@ def import_from_file(file_path):
                 headers = next(reader)  # skip the header row
                 employee_data_list = [row for row in reader]
             return employee_data_list
-        except tuple(error_messages.keys()) as e:
-            print(error_messages[type(e)] % str(e))
+        except FileNotFoundError:
+            print(f"Error: file {file_path} not found.")
+        except csv.Error as e:
+            print(f"Error reading CSV file {file_path}: {e}")
         except Exception as e:
-            print(error_messages[Exception] % str(e))
+            print(f"Error reading file {file_path}: {e}")
 
-    elif file_extension in ('.xlsx', '.xls'):
+    elif file_extension == '.xlsx' or file_extension == '.xls':
         try:
-            with pd.ExcelFile(file_path, engine='openpyxl') as xls:
-                sheet_name = xls.sheet_names[0]
-                employee_data = pd.read_excel(xls, sheet_name)
-                employee_data_list = employee_data.values.tolist()
+            employee_data = pd.read_excel(file_path, engine='openpyxl', skiprows=[0])
+            employee_data_list = employee_data.values.tolist()
             return employee_data_list
-        except tuple(error_messages.keys()) as e:
-            print(error_messages[type(e)] % str(e))
+        except FileNotFoundError:
+            print(f"Error: file {file_path} not found.")
         except Exception as e:
-            print(error_messages[Exception] % str(e))
-
+            print(f"Error reading Excel file {file_path}: {e}")
     else:
         print(f"Error: file {file_path} is not a CSV or Excel file.")
         return None
 
 
 
-def create_file():
-    # imports employee data from a CSV file
-    employee_data_list = import_from_file()
+def create_file(file_path, overwrite=False, verbose=False):
+
+    # imports employee data from a file
+    employee_data_list = import_from_file(file_path)
 
     # handle errors during data import
     if employee_data_list is None:
         print("Error importing employee data. File may be empty or malformed.")
         return
 
+    # check if the output file already exists
+    if os.path.isfile(fl.File_name() + ".txt") and not overwrite:
+        print("Error: output file already exists. Use the --overwrite option to overwrite the file.")
+        return
+
     # write employee data to a file
-    file_name = fl.File_name() + ".txt"
-    with open(file_name, "w+") as file:
+    mode = 'w' if overwrite else 'x'
+    with open(fl.File_name() + ".txt", mode) as file:
         file.write(fl.Header() + "\n")
         for employee_data in employee_data_list:
             file.write(fl.employee_remunaration_details(employee_data) + "\n")
-            print(f"{employee_data[1]} was recorded in file with INSS standards.\n")
+            if verbose:
+                print(f"{employee_data[1]} was recorded in file with INSS standards.")
         file.write(fl.finalization_of_file_information())
-        print(f"File {file_name} successfully created.")
+
+    print(f"File {fl.File_name() + '.txt'} successfully created.")
+
+file_path = select_file()
