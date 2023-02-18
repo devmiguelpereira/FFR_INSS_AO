653321dd655b049454a95669fd38b465dc40c2eb
EIS-MPEREIRA <101540483+EIS-MPEREIRA@users.noreply.github.com>
2023-02-18 08:04:39 +0100

    Improved import of data and final file creation

--- python_source_code/FileHandler.py
+++ python_source_code/FileHandler.py
@@ -1,10 +1,39 @@
+import os
+import csv
 import FileLayout as fl
 
+file_path = 'november_inss_2022.csv'
 
-def Criar_Ficheiro():
-    # Abre um ficheiro no formato txt para escrever e criar se não existir
-    ficheiro = open(fl.Nome_Ficheiro()+".txt", "w+")
+# imports data from a csv file
 
-    ficheiro.write(fl.Cabecalho_Ficheiro()+"\n")
-    ficheiro.write(fl.Remuneracao_Funcionario_Detalhe()+"\n")
-    ficheiro.write(fl.Totalizador_Ficheiro())
+
+def importFromFile(file_path=file_path):
+    set = []
+
+    with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
+        valueLines = csv.reader(csvfile, delimiter=',')
+        for line in valueLines:
+            set.append(line)
+
+    #set.pop(0)  # deleting the first line with headers
+
+    return set
+
+
+def create_file():
+    # tries to Open or creates a txt file write the data collected
+    _EmployeesData = importFromFile()
+
+    print(type(_EmployeesData))
+
+    file = open(fl.File_name()+".txt", "w+")
+
+    file.write(fl.Header()+"\n")
+
+    for Employee in _EmployeesData:
+        # print(Employee,"Employee Data \n")
+
+        file.write(fl.employee_remunaration_details(Employee)+"\n")
+        print(Employee[1] +" was recorded in file with inss standards \n")
+
+    file.write(fl.finalization_of_file_information())
